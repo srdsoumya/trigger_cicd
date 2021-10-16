bbac3fc3e3410d90ef00c0e80acffd7bb2e02976
Jenkins <jenkins@ip-10-0-0-143.ec2.internal>
2021-10-16 19:08:35 +0000

    Input param updated dtv

--- vars.tf
+++ vars.tf
@@ -1,23 +1,23 @@
 variable "mots_id" {
-	default = "infy_mots_id"
+	default = "321"
 }
 
 variable "owned_by" {
-	default = "infy_owned_by"
+	default = "dtv"
 }
 
 variable "cluster_name" {
-	default = "infy_cluster_name"
+	default = "skava_prod"
 }
 
 variable "vpc_id" {
-	default = "infy_vpc_id"
+	default = "vpc-0d066b274bd6c05a3"
 }
 
 variable "subnet_ids" {
-	default = infy_subnet_ids
+	default = ["subnet-09cc53a07375ef0fc","subnet-09b7a28d8e15b9684","subnet-0fcabe6f1b0ded6cc","subnet-0a769de4abf41a046"]
 }
 
 variable "no_of_node" {
-	default = infy_no_of_node
+	default = 2
 }
