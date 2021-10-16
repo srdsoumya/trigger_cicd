0d38fcdc1911aee1e2237b1cacb207fd870359e0
srdsoumya <soumyadas755@gmail.com>
2021-10-17 00:08:17 +0530

    AWS Provider file updated

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  required_version = ">= 1.0.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
   backend "s3" {
     bucket = "mybucktest755"
     key    = "development/terraform/aws-s3/terraform.tfstate"
